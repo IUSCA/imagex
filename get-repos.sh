656063ee59548bb6efa77825e812eb05efb3edb6
Michael Young <youngmd@iu.edu>
2018-06-13 14:10:00 +0000

    Updated docker-compose and added placeholders to include empty folders in repo

---
+++ get-repos.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/IUSCA/imagex-api.git
+git clone https://github.com/IUSCA/imagex-ui.git
+git clone https://github.com/IUSCA/imagex-data.git
+git clone https://github.com/IUSCA/imagex-importer.git
